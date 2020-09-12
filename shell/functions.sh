67453c484c4ac44196c8584a46785c6909fe4868
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2020-09-12 15:02:31 +0200

    rm dotfiles-update and connect-the-dots functions

--- shell/functions.sh
+++ shell/functions.sh
@@ -11,24 +11,3 @@ path_prepend() {
     path_remove "$1"
     PATH="$1${PATH:+":$PATH"}"
 }
-
-# Install lazydotfiles
-connect_the_dots() {
-    if [ ! -d "$HOME/.lazy-dotfiles" ]; then
-        gcl https://github.com/danielfalbo/lazy-dotfiles ~/.lazy-dotfiles
-    fi
-    cd ~/.lazy-dotfiles
-    gl --ff-only
-    ./install
-}
-
-# Update dotfiles
-dfu() {
-    cd ~/.dotfiles
-    gl --ff-only
-    ./install
-    if [ -d "$HOME/.lazy-dotfiles" ]; then
-        connect_the_dots
-    fi
-}
-
