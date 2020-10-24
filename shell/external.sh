f178b69c10a02ebdfaa379426e5c7fefa2288538
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2020-10-24 17:36:13 +0200

    add tldr and most

--- shell/external.sh
+++ shell/external.sh
@@ -1,2 +1,5 @@
 # fzf settings
 export FZF_DEFAULT_OPTS="--height 90% --layout=reverse --border --preview 'bat --style=numbers --color=always --line-range :500 {}' --color 'bg+:-1'"
+
+# pager
+export PAGER="most"
